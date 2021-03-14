2e5b8896ffd4f1170e9823d5762dc99f41ce5809
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2021-03-14 12:37:41 +0300

    IMplement offer filter & search

--- offer_yangu/offers/admin.py
+++ offer_yangu/offers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Offers
+from .models import Category, Offers, Location
 
 # Register your models here.
 admin.site.register(Offers)
 admin.site.register(Category)
+admin.site.register(Location)
--- offer_yangu/offers/models.py
+++ offer_yangu/offers/models.py
@@ -2,20 +2,29 @@ from django.db import models
 from offer_yangu.authentication.models import User
 
 # Create your models here.
+
+
 class Offers(models.Model):
-  name_of_product =  models.CharField(max_length=255, unique=True)
-  start_date = models.DateTimeField()
-  end_date = models.DateTimeField()
-  stock = models.IntegerField(null=True)
-  location = models.CharField(max_length=255)
-  description = models.TextField()
-  seller = models.ForeignKey(User, on_delete=models.CASCADE)
-  previous_price = models.FloatField()
-  current_price = models.FloatField()
-  category = models.ForeignKey("Category", verbose_name=("offer_category"), on_delete=models.CASCADE)
-  image = models.ImageField(null=True)
-  approved = models.BooleanField(default=False)
+    name_of_product = models.CharField(max_length=255, unique=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    stock = models.IntegerField(null=True)
+    description = models.TextField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    previous_price = models.FloatField()
+    current_price = models.FloatField()
+    category = models.ForeignKey("Category", verbose_name=("offer_category"),
+                                 on_delete=models.CASCADE)
+    location = models.ForeignKey("Location", verbose_name=("offer_location"),
+                                  on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(null=True)
+    approved = models.BooleanField(default=False)
+    top_offer = models.BooleanField(default=False)
 
 
 class Category(models.Model):
-  name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255, unique=True)
